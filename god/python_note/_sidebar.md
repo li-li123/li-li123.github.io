2f6992b65b8e8c7f4021760a7a7f70f286a001d8
NingWang Code Server <yogehaoren@gmail.com>
2023-03-11 21:11:58 +0800

    drf

--- god/python_note/_sidebar.md
+++ god/python_note/_sidebar.md
@@ -1,3 +1,4 @@
 * [字典
   ](god/python_note/README.md)
 * [列表](god/python_note/list.md)
+* 继承
--- god/python_note/list.py
+++ god/python_note/list.py
@@ -22,12 +22,11 @@ class B(A):   #B继承A就是B继承A中所有的A的属性和方法
     # name = 'B'
 
     def printClassName(self):
-        # self.name = 'B'   # B继承了A之后，A中已经拥有的变量名在B中只需要继承使用即可，B中的变量名尽量减少和A中变量名重复
+        self.name = 'B'   # B继承了A之后，A中已经拥有的变量名在B中只需要继承使用即可，B中的变量名尽量减少和A中变量名重复
         # print(self.name)
         print(super().name)  #super（）就是找父类·
         print("======super=======")
         super().printClassName()  #直接继承A中的方法
-
         print("=================")
         # print('B')
 
@@ -42,15 +41,16 @@ if __name__ == '__main__':
     # a_obj = A()
     # a_obj.printClassName()
     # a_obj_2 = A()
-    a_obj_2.name = "wangningsb"
-    a_obj_2.age = 18
-    a_obj_2.printClassName()
+    # a_obj_2.name = "wangning"
+    # a_obj_2.age = 18
+    # a_obj_2.printClassName()
 
     b_obj = B()
-    b_obj.printClassName()
+    # b_obj.printClassName()
     # B.printClassName(b_obj)
     # A.printClassName(b_obj)
     # print("=====")
-    # A.printClassName(super(B, b_obj))
+
+    A.printClassName(super(B, b_obj))  #super(B，b_obj）super（）就是求某个参数的父类，像本句中就是求B的父类，然后将b_obj强转为B的父类即A
     # print("======")
     # A.printClassName(b_obj)
