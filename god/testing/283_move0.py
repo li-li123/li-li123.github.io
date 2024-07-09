81c4bfa916321055b67eaec1cd3b9b75aed5f77f
abadfox <yogehaoren@gmail.com>
2024-07-09 22:46:16 +0800

    update

--- god/testing/283_move0.py
+++ god/testing/283_move0.py
@@ -1,27 +1,22 @@
+
 class Solution:
     def moveZeroes(self, nums):
-
         if len(nums) == 0:
             return []
         k = 0
         while k <len(nums):
-            if nums[k] == 0:
-                break
-            k = k+1
+            if nums[k]==0:
+                break     
+            k=k+1
         for i in range(k+1,len(nums)):
             if nums[i] != 0:
                 nums[k] = nums[i]
                 nums[i]=0
-                while k <len(nums):
-                    if nums[k] == 0:
+                while k<len(nums):
+                    if nums[k]==0:
                         break
                     k = k+1
         return nums
-
-                
-            
-
-            
             
                 
 
@@ -30,6 +25,6 @@ class Solution:
 
 
 if __name__ == '__main__':
-    nums = [0,1,2,3,4,0,8,3,0]
+    nums = [1,0,0,3,4,0,8,3,0]
     A = Solution()
     print(A.moveZeroes(nums))
